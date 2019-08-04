855412287549a53527789709d74983d9a303e49c
Jeff Hiltz <jeffhiltz@gmail.com>
2019-08-04 13:51:43 +0200

    try deriving the other color values

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,31 @@
-import { writable } from 'svelte/store'
+import { derived, writable } from 'svelte/store'
+import convert from 'color-convert'
 
 export const colors = writable([])
 export const background = writable('#000000')
+
+export const colorValues = derived(
+  colors,
+  $colors => {
+    const values = {}
+    $colors.forEach(hex => {
+      const rgb = convert.hex.rgb(hex)
+      const lab = convert.hex.lab(hex)
+      const hsl = convert.hex.hsl(hex)
+      values[hex] = {
+        hex: hex,
+        red: rgb[0],
+        green: rgb[1],
+        blue: rgb[2],
+        hue: hsl[0],
+        saturation: hsl[1],
+        hsLightness: hsl[2],
+        lightness: lab[0],
+        aStar: lab[1],
+        bStar: lab[2],
+      }
+    })
+    return values
+  }
+)
 
