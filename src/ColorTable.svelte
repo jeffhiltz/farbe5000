b0e07a934051cd7975f7480e6fb7fa30f417982b
Jeff Hiltz <jeffhiltz@gmail.com>
2019-08-04 16:15:12 +0200

    show colorValues in table

--- src/ColorTable.svelte
+++ src/ColorTable.svelte
@@ -1,18 +1,23 @@
 <script>
-  import { colors } from './colors.js'
-//  const convert = require('color-convert')
+  import { colors, colorValues } from './colors.js'
 </script>
 
 <table>
   <thead>
     <tr>
       <th>Hex</th>
+      <th>Lightness</th>
+      <th>aStar</th>
+      <th>bStar</th>
     </tr>
   </thead>
   <tbody>
     {#each $colors as hex}
       <tr>
-        <td>{hex}</td>
+        <td>{$colorValues[hex].hex}</td>
+        <td>{$colorValues[hex].lightness}</td>
+        <td>{$colorValues[hex].aStar}</td>
+        <td>{$colorValues[hex].bStar}</td>
       </tr>
     {/each}
   </tbody>
