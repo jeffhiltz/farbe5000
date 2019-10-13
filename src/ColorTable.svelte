d8001c4d9b52f46ddcfa3ffa5b0042d5ae23a2b2
Jeff Hiltz <jeffhiltz@gmail.com>
2019-10-13 21:40:08 +0200

    get primitive updating of colors from table

--- src/ColorTable.svelte
+++ src/ColorTable.svelte
@@ -1,8 +1,16 @@
 <script>
   import { colors, colorValues } from './colors.js'
+  import convert from 'color-convert'
 
   function handleChange(e) {
-    console.log(e)
+    // console.log(e)
+    const colorId = parseInt(e.target.getAttribute('color_id'))
+    console.log($colors[colorId])
+    // $colors = [...$colors.slice(0, colorId), {id: colorId, hex: '#fe57a1'}, ...$colors.slice(colorId + 1)]
+    colors.update(cols => {
+      cols[colorId].hex = '#beef99'
+      return cols
+    })
   }
 
 </script>
@@ -17,11 +25,11 @@
     </tr>
   </thead>
   <tbody>
-    {#each $colorValues as value (value.hex)}
+    {#each $colorValues as value, idx (value.id)}
       <tr>
         <td>{value.hex}</td>
-        <td><input on:change="{handleChange}" value="{value.lightness}"></td>
-        <td><input on:change="{handleChange}" value="{value.aStar}"></td>
+        <td><input color_id={value.id} on:change="{handleChange}" value="{value.lightness}"></td>
+        <td><input color_id={value.id} on:change="{handleChange}" value="{value.aStar}"></td>
         <td>{value.bStar}</td>
       </tr>
     {/each}
