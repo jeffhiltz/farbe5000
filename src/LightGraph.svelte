a3b7118683a271b45048d6acc85a0cb2543bc1ff
Jeff Hiltz <jeff.hiltz@vimcar.com>
2021-02-06 12:25:26 +0100

    added numbers to y-axis and words to x-axis. wip

--- src/LightGraph.svelte
+++ src/LightGraph.svelte
@@ -1,10 +1,10 @@
 <script>
   import { colors, colorValues, backHex } from './colors.js'
 
-  let width = 500
-  let height = 500
-  $: barSpace = width / $colors.length
-  $: barWidth = barSpace * 0.75
+  let width = 95
+  let height = 95
+  $: barSpace = width / $colors.length // width of the bar including padding
+  $: barWidth = barSpace * 0.75 // width without padding
 
   $: bars = $colors.map(color => {
     return {
@@ -12,6 +12,11 @@
       lightness: $colorValues[color.id].lightness,
     }
   })
+
+  // move to a utility class?
+  function range(size, startAt = 0) {
+    return [...Array(size).keys()].map(i => i + startAt)
+  }
 </script>
 
 <style>
@@ -21,12 +26,23 @@
     max-height: 49.5vh;
     margin: 0 0 0.5vh 0;
   }
+
+  .labels {
+    font-size: 0.22vh;
+  }
 </style>
 
-<svg viewBox="0 0 500 500">
-  <rect width="100%" height="100%" fill="{$backHex}"></rect>
-  {#each bars as {hex, lightness}, idx}
+<svg viewBox="0 0 100 100">
+  <svg x="5" y="5">
+    <rect width="95" height="95" fill="{$backHex}"></rect>
+    {#each bars as {hex, lightness}, idx}
     <rect width="{barWidth}" height="{lightness}%" fill="#{hex}" x="{idx * barSpace + (barSpace / 2) - (barWidth / 2)}" y="{100 - lightness}%"></rect>
-  {/each}
+    {/each}
+  </svg>
+  <g class="labels">
+    {#each range(4, 1) as i}
+    <text x="0" y="{95/5 * i + 7}">{100 - (i * 1/5 * 100)}</text>
+    {/each}
+  </g>
 </svg>
 
--- src/ScatterGraph.svelte
+++ src/ScatterGraph.svelte
@@ -1,14 +1,14 @@
 <script>
   import { colors, colorValues, backHex } from './colors.js'
 
-  let width = 500
-  let height = 500
-  const pointWidth = 20
-  const pointHeight = 20
-  const maxAValue = 100
-  const minAValue = -100
-  const maxBValue = 100
-  const minBValue = -100
+  let width = 95
+  let height = 95
+  const pointWidth = 4
+  const pointHeight = 4
+  const maxAValue = 95
+  const minAValue = -95
+  const maxBValue = 95
+  const minBValue = -95
 
   // Calculate the position of a value within a range
   function calculatePercentage(value, min, max) {
@@ -22,6 +22,10 @@
       bPercent: calculatePercentage($colorValues[color.id].bStar, minBValue, maxBValue),
     }
   })
+
+  function range(size, startAt = 0) {
+    return [...Array(size).keys()].map(i => i + startAt)
+  }
 </script>
 
 <style>
@@ -31,12 +35,27 @@
     max-height: 49.5vh;
     margin: 0.5vh 0 0 0;
   }
+
+  .labels {
+    font-size: 0.22vh;
+  }
 </style>
 
-<svg viewBox="0 0 500 500">
-  <rect width="100%" height="100%" fill="{backHex}"></rect>
-  {#each points as {hex, aPercent, bPercent}, idx}
+<svg viewBox="0 0 100 100">
+  <svg x="5">
+    <rect width="95" height="95" fill="{backHex}"></rect>
+    {#each points as {hex, aPercent, bPercent}, idx}
     <ellipse rx="{pointWidth / 2}" ry="{pointWidth / 2}" fill="#{hex}" cx="{aPercent * width}" cy="{bPercent * height}"></ellipse>
-  {/each}
+    {/each}
+  </svg>
+  <g class="labels">
+    {#each range(7, 1) as i}
+    <text x="0" y="{95/8 * i + 2}">{(i * 1/8 * 200) - 100}</text>
+    {/each}
+  </g>
+  <g class="labels">
+    <text x="25" y="98.5">Green</text>
+    <text x="75" y="98.5">Red</text>
+  </g>
 </svg>
 
