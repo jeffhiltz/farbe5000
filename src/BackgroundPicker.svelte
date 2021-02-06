d864d90fb4776129758bea92edfbd57015f12a06
Jeff Hiltz <jeff.hiltz@vimcar.com>
2021-02-06 10:21:46 +0100

    nest input inside label

--- src/BackgroundPicker.svelte
+++ src/BackgroundPicker.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <h2>Background Colour</h2>
-<label>Lightness:</label><input type=number min=0 max=100 bind:value={$bgLightness}>
-<label>aStar:</label><input type=number min=-86 max=98 bind:value={$bgAStar}>
-<label>bStar:</label><input type=number min=-108 max=94 bind:value={$bgBStar}>
+<label>Lightness:<input type=number min=0 max=100 bind:value={$bgLightness}></label>
+<label>aStar:<input type=number min=-86 max=98 bind:value={$bgAStar}></label>
+<label>bStar:<input type=number min=-108 max=94 bind:value={$bgBStar}></label>
